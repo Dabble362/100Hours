307a568554666815e386ea753bb7ad174d63bdb4
Dabble362 <75595585+Dabble362@users.noreply.github.com>
2022-09-26 10:22:34 -0400

    add shifts controller, schedule route

--- routes/main.js
+++ routes/main.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth");
 const homeController = require("../controllers/home");
+const shiftsController = require("../controllers/shifts");
 const { ensureAuth, ensureGuest } = require("../middleware/auth");
 
 //Main Routes - simplified for now
@@ -11,6 +12,6 @@ router.post("/login", authController.postLogin);
 router.get("/logout", authController.logout);
 router.get("/signup", authController.getSignup);
 router.post("/signup", authController.postSignup);
-router.get("/schedule", authController.getSchedule);
+router.get("/schedule", ensureAuth, shiftsController.getSchedule);
 
 module.exports = router;
